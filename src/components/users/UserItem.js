c6bc4103f25648476819c22caa0aa93eb9fbbad2
JeremiahMey <jmey9300@gmail.com>
2019-11-13 13:01:13 -0700

    v1.4

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react'
+import React from "react";
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-  render() {
-    return (
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
       <div>
-        User Item
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
       </div>
-    )
-  }
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
 }
 
-export default UserItem
+export default UserItem;
