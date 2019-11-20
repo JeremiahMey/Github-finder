9e6fd20fd35bedb37d077926313e8fd7ba39ab59
JeremiahMey <jmey9300@gmail.com>
2019-11-20 13:27:03 -0700

    V 4

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types'
-
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ icon, title }) => {
-  
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className={icon}></i>
-          {title}</h1>
-      </nav>
-    )
-  
-}
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon}></i>
+        {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 Navbar.defaultProps = {
-  title: 'Github Finder',
-  icon: 'fab fa-github'
+  title: "Github Finder",
+  icon: "fab fa-github"
 };
 
 Navbar.propTypes = {
@@ -24,4 +31,4 @@ Navbar.propTypes = {
   icon: PropTypes.string.isRequired
 };
 
-export default Navbar
+export default Navbar;
